634b938309a3e29822f202a9e482a64257fc4639
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-22 14:57:20 +0530

    done

--- index.js
+++ index.js
@@ -14,7 +14,6 @@
          console.log(moviesdata)
         //   display(moviesdata)
         return moviesdata
-         console.log("data:",data);
  
         } catch(error){
             console.log("error:",error);
@@ -50,4 +49,38 @@ function deboounce(func,delay){
    timerid= setTimeout(function(){
         func()// still the main()  function
     },delay)
+}
+
+async function searchMovies(){
+    var urls=`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e01cb6bcec8a3835a9b58765a46588f1`
+    try {
+        let input=document.getElementById("submit").value;
+
+     let res = await fetch(urls);
+     let data =await res.json();
+     var moviesdatas=data.results
+     console.log(moviesdatas)
+         display1(moviesdatas)
+
+    } catch(error){
+        console.log("error:",error);
+    }
+ }
+ searchMovies()
+let a=`https://image.tmdb.org/t/p/w500`
+
+ function display1(datas){
+    datas.map(function (elem ,index){
+      let div=document.createElement("div")
+      let img=document.createElement("img")
+       img.src=`${a}${elem.poster_path}`
+       let h3=document.createElement("h3") 
+        h3.innerText= "Title:"+"  "+elem.original_title
+        h3.style.color="white"
+       let p=document.createElement("p")
+       p.innerText="Imbd_reating:"+"  "+elem.vote_average
+       div.append(img,h3,p)
+       document.getElementById("append").append(div)
+
+    })
 }
